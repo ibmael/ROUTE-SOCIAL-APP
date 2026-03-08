4c8c8b0e15fdc7b822cb27cc2dd9dff7d52ba232
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-03-09 00:25:14 +0200

    FINISHING WITH THE REGISTER VALIDATION FORM

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [ ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-registerForm:FormGroup =new FormGroup({
-  name: new FormControl("",[Validators.required,Validators.minLength(3)]),
-  username: new FormControl(""),
-  email: new FormControl("",[Validators.required,Validators.email]),
-  dateOfBirth: new FormControl("",[Validators.required]),
-  gender: new FormControl("Male",[Validators.required]),
-  password: new FormControl("",[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]),
-  rePassword: new FormControl("",[Validators.required])
-});
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    username: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    dateOfBirth: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d).{6,}$/),
+    ]),
+    rePassword: new FormControl('', [Validators.required]),
+  });
 
-submitForm():void{
-  console.log(this.registerForm);
-
-}
+  submitForm(): void {
+    console.log(this.registerForm.value);
+  }
 }
