06709a6843fa8f29139ac18f36c2b08add1233cb
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-03-26 20:26:44 +0200

    FINISHING THE SIGNIN & REGISTERATION LOGIC

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/auth/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private readonly authService = inject(AuthService);
+  loginSubscribe: Subscription = new Subscription();
+  isLoading: boolean = false;
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d).{6,}$/),
+    ]),
+  });
+  loginSubmit(): void {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      this.loginSubscribe.unsubscribe();
+      this.loginSubscribe = this.authService.signIn(this.loginForm.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.log(err);
+          this.isLoading = false;
+        },
+      });
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, Inject, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/auth/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -9,20 +17,53 @@ import { RouterLink } from '@angular/router';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  registerForm: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    username: new FormControl(''),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    dateOfBirth: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d).{6,}$/),
-    ]),
-    rePassword: new FormControl('', [Validators.required]),
-  });
+  private readonly authService = inject(AuthService);
+  messError: string = '';
+  isLoading: boolean = false;
+  registerSubscribe: Subscription = new Subscription();
 
+  registerForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      username: new FormControl('', [Validators.required, Validators.pattern(/^[a-z0-9_]{3,30}$/)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      dateOfBirth: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d).{6,}$/),
+      ]),
+      rePassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    },
+    { validators: this.confirmPassword },
+  );
+
+  confirmPassword(group: AbstractControl) {
+    const password = group.get('password')?.value;
+    const rePassword = group.get('rePassword')?.value;
+    if (password !== rePassword && rePassword !== '') {
+      group.get('rePassword')?.setErrors({ mismatch: true });
+      return { mismatch: true };
+    }
+    return null;
+  }
   submitForm(): void {
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      this.isLoading = true;
+      this.registerSubscribe.unsubscribe();
+      this.registerSubscribe = this.authService.signUp(this.registerForm.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.log(err);
+          this.messError = err.error.message;
+          this.isLoading = false;
+        },
+      });
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
   }
 }
